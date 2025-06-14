71b5a1db0f8fa19facb5a2b305b834a098a59587
Gilad Weinberger <giladwe21@gmail.com>
2025-06-14 23:34:05 +0300

    Refactor PostHog integration: replace Providers with PostHogProvider in layout.jsx and implement PostHogProvider in providers.jsx

--- app/providers.jsx
+++ app/providers.jsx
@@ -1,6 +1,25 @@
 "use client";
-import { AuthProvider } from "@/context/AuthContext";
 
-export function Providers({ children }) {
-  return <AuthProvider>{children}</AuthProvider>;
+import { AuthProvider } from "@/context/AuthContext";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+import { usePostHog } from "posthog-js/react";
+import posthog from "posthog-js";
+import { PostHogProvider as PHProvider } from "posthog-js/react";
+
+export function PostHogProvider({ children }) {
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+      api_host:
+        process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
+      person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+      defaults: "2025-05-24",
+    });
+  }, []);
+
+  return (
+    <PHProvider client={posthog}>
+      <AuthProvider>{children}</AuthProvider>
+    </PHProvider>
+  );
 }
