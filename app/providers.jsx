b505e443a14dc46aeb4ed1cbca6c33caabf42429
Gilad Weinberger <giladwe21@gmail.com>
2025-06-15 10:55:28 +0300

    Remove PostHogProvider from Providers component in providers.jsx, simplifying the structure and enhancing code clarity by relying solely on AuthProvider.

--- app/providers.jsx
+++ app/providers.jsx
@@ -1,20 +1,7 @@
 "use client";
-import { AuthProvider } from "@/context/AuthContext";
-import { PostHogProvider } from "posthog-js/react";
-import posthog from "posthog-js";
 
-if (typeof window !== "undefined") {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host:
-      process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
-    person_profiles: "identified_only", // or always to track anonymous users
-  });
-}
+import { AuthProvider } from "@/context/AuthContext";
 
 export function Providers({ children }) {
-  return (
-    <PostHogProvider client={posthog}>
-      <AuthProvider>{children}</AuthProvider>
-    </PostHogProvider>
-  );
+  return <AuthProvider>{children}</AuthProvider>;
 }
